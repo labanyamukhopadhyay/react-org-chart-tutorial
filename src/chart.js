718960304c469231b75d7ddd87d215e9d3dcf2ed
Labanya Mukhopadhyay <labanyam@hotmail.com>
2021-12-05 04:36:21 -0800

    resolve chart file

--- src/chart.js
+++ src/chart.js
@@ -6,11 +6,7 @@ import React, { Fragment } from "react";
 import data from "./data.json";
 
 const Card = (props) => {
-<<<<<<< HEAD
   //const levelColor = randomcolor();
-=======
-  const levelColor = randomcolor();
->>>>>>> 07b789fc2826942bef5e819b3178c9912369a10c
 
   return (
     <ul>
@@ -18,7 +14,6 @@ const Card = (props) => {
         <Fragment key={item.name}>
           <li>
             <div className="card">
-<<<<<<< HEAD
               {/* <div className="image">
                 <img
                   //src={faker.image.avatar()}
@@ -32,20 +27,6 @@ const Card = (props) => {
               {/* <div className="card-footer" style={{ 
                 //background: levelColor 
                 }}>
-=======
-              <div className="image">
-                <img
-                  src={faker.image.avatar()}
-                  alt="Profile"
-                  style={{ borderColor: levelColor }}
-                />
-              </div>
-              <div className="card-body">
-                <h4>{faker.name.findName()}</h4>
-                <p>{faker.name.jobTitle()}</p>
-              </div>
-              <div className="card-footer" style={{ background: levelColor }}>
->>>>>>> 07b789fc2826942bef5e819b3178c9912369a10c
                 <img
                   src="https://www.flaticon.com/svg/static/icons/svg/2950/2950657.svg"
                   alt="Chat"
@@ -58,11 +39,7 @@ const Card = (props) => {
                   src="https://www.flaticon.com/svg/static/icons/svg/570/570387.svg"
                   alt="Video"
                 />
-<<<<<<< HEAD
               </div> */}
-=======
-              </div>
->>>>>>> 07b789fc2826942bef5e819b3178c9912369a10c
               <div></div>
             </div>
             {item.children?.length && <Card data={item.children} />}
@@ -81,8 +58,4 @@ const Chart = () => {
   );
 };
 
-<<<<<<< HEAD
 export default Chart;
-=======
-export default Chart;
->>>>>>> 07b789fc2826942bef5e819b3178c9912369a10c
