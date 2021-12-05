7476ed406ed97195df6fa70f5167aabea44c3430
Labanya Mukhopadhyay <labanyam@hotmail.com>
2021-12-05 04:13:32 -0800

    update chart

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./styles.css";
+import Chart from "./chart";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+        DSS Lineage Hierarchy Chart
+        {/* <span className="follow">
+          <a href="https://twitter.com/naveenda_">
+            <img
+              alt="Follow @naveenda_"
+              src="https://www.freepnglogos.com/uploads/twitter-logo-png/twitter-logo-vector-png-clipart-1.png"
+            />
+            Follow @naveenda_
+          </a>
+        </span> */}
+      </div>
+      <div className="container">
+        <Chart />
+      </div>
     </div>
   );
 }
-
-export default App;
